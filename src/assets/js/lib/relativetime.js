9f2cdd848b7210f7d45c5343cd74994678534db5
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2021-09-04 21:43:00 +0800

    :sparkles: Stop using private fields to support older browsers.

--- src/assets/js/lib/relativetime.js
+++ src/assets/js/lib/relativetime.js
@@ -4,11 +4,8 @@
 // https://stackoverflow.com/a/53800501/13319205
 
 class RelativeTime {
-  #relativeTimeUnits
-  #relativeTimeFormatter
-
   constructor (language) {
-    this.#relativeTimeUnits = {
+    this.relativeTimeUnits = {
       year  : 24 * 60 * 60 * 1000 * 365,
       month : 24 * 60 * 60 * 1000 * 365 / 12,
       day   : 24 * 60 * 60 * 1000,
@@ -17,10 +14,10 @@ class RelativeTime {
       second: 1000
     }
 
-    this.#relativeTimeFormatter = this.#getNewFormatter(language)
+    this.relativeTimeFormatter = this.getNewFormatter(language)
   }
 
-  #getNewFormatter(language) {
+  getNewFormatter(language) {
     return new Intl.RelativeTimeFormat(language, {
       localeMatcher: "best fit",
       numeric: "always",
@@ -29,16 +26,16 @@ class RelativeTime {
   }
 
   setLanguage (language) {
-    this.#relativeTimeFormatter = this.#getNewFormatter(language)
+    this.relativeTimeFormatter = this.getNewFormatter(language)
   }
 
   getRelativeTime (d1, d2=new Date()) {
     const diff = d1 - d2
 
-    for (const unit in this.#relativeTimeUnits) {
-      if (Math.abs(diff) > this.#relativeTimeUnits[unit] || unit === "second") {
-        return this.#relativeTimeFormatter.format(
-          Math.round(diff / this.#relativeTimeUnits[unit]), 
+    for (const unit in this.relativeTimeUnits) {
+      if (Math.abs(diff) > this.relativeTimeUnits[unit] || unit === "second") {
+        return this.relativeTimeFormatter.format(
+          Math.round(diff / this.relativeTimeUnits[unit]), 
           unit
         )
       }
